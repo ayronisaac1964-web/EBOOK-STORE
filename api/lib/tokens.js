d2a80d85a272b85ef0c918dbbf8b32a108e6e905
ayronisaac1964-web <ayronisaac1964@gmail.com>
2026-07-12 08:34:50 +1000

    Delete api directory

--- api/lib/tokens.js
+++
@@ -1,8 +0,0 @@
-const crypto = require("crypto");
-
-// 32 bytes -> 43-char url-safe base64. Not guessable, no book/order id leaked.
-function generateToken() {
-  return crypto.randomBytes(32).toString("base64url");
-}
-
-module.exports = { generateToken };
